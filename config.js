e9547047abbafe9b5a583fa570cce6ce9c980ff6
Moth <tdrice668@gmail.com>
2024-12-01 21:10:25 -0600

    added config.js file for render database, also reattached README.md

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres',
+    },
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres',
+    }
+  };
+  
